e842a862c2819b78d7c0895382d96c962bf49031
KireinaHoro <i@jsteward.moe>
2019-06-01 11:49:47 +0800

    refactor template tuner

--- auto_schedule/auto_schedule.py
+++ auto_schedule/auto_schedule.py
@@ -20,33 +20,24 @@ def auto_schedule(func, args):
     """
 
     ops, bufs = func(*args)
-    #################################################
-    # do some thing with `ops`, `bufs` and `args`
-    # to analyze which schedule is appropriate
-
-    # TODO: replace this with autoschedule
-    s = tvm.create_schedule(ops)
 
     @autotvm.template
     def gemm():
         assert len(bufs) == 3
         s = tvm.create_schedule(ops)
-
         C = bufs[2]
 
         _, y, x = s[C].op.axis
         k = s[C].op.reduce_axis[0]
-        ko, ki = s[C].split(k, factor=4)
 
         cfg = autotvm.get_config()
 
-        #cfg.define_split("split_k", k, num_outputs=2)
+        cfg.define_split("split_k", k, num_outputs=2)
         cfg.define_split("tile_y", y, num_outputs=2)
         cfg.define_split("tile_x", x, num_outputs=2)
-        ##### define space end #####
 
         # schedule according to config
-        #ko, ki = cfg["split_k"].apply(s, C, k)
+        ko, ki = cfg["split_k"].apply(s, C, k)
         yo, yi = cfg["tile_y"].apply(s, C, y)
         xo, xi = cfg["tile_x"].apply(s, C, x)
 
@@ -55,45 +46,43 @@ def auto_schedule(func, args):
 
         return s, bufs
 
+    @autotvm.template
+    def conv():
+        s = tvm.create_schedule(ops)
+        return s, bufs
+
     if len(args) == 4:
-        task = autotvm.task.create(gemm, args=(), target='llvm')
-        print(task.config_space)
-
-        logging.getLogger('autotvm').setLevel(logging.DEBUG)
-        logging.getLogger('autotvm').addHandler(logging.StreamHandler(sys.stdout))
-
-        measure_option = autotvm.measure_option(
-            builder='local',
-            runner=autotvm.LocalRunner(number=5)
-        )
-
-        print('=====begin tuning for ' + str(args))
-        filename = 'gemm.log'
-        if os.path.exists(filename):
-            os.remove(filename)
-
-        tuner = autotvm.tuner.GATuner(task)
-        tuner.tune(n_trial=20,
-                   measure_option=measure_option,
-                   callbacks=[autotvm.callback.log_to_file(filename)])
-
-        print('=====finish tuning for ' + str(args))
-
-        with autotvm.apply_history_best(filename):
-            with tvm.target.create("llvm"):
-                return gemm()
-
+        template = gemm
     elif len(args) == 13:
-        print('conv2d')
+        template = conv
     else:
         print('unknown operation')
-    
-    #################################################
-    # perform real schedule according to 
-    # decisions made above, using primitives 
-    # such as split, reorder, parallel, unroll...
-    
-    #################################################
-    # finally, remember to return these two results
-    # we need `bufs` to build function via `tvm.build`
-    return s, bufs
+        s = tvm.create_schedule(ops)
+        return s, bufs
+
+    task = autotvm.task.create(template, args=(), target='llvm')
+    print(task.config_space)
+
+    logging.getLogger('autotvm').setLevel(logging.DEBUG)
+    logging.getLogger('autotvm').addHandler(logging.StreamHandler(sys.stdout))
+
+    measure_option = autotvm.measure_option(
+        builder='local',
+        runner=autotvm.LocalRunner(number=5)
+    )
+
+    print('=====begin tuning for ' + str(args))
+    filename = 'tune.log'
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    tuner = autotvm.tuner.XGBTuner(task, loss_type='rank')
+    tuner.tune(n_trial=100,
+               measure_option=measure_option,
+               callbacks=[autotvm.callback.log_to_file(filename)])
+
+    print('=====finish tuning for ' + str(args))
+
+    with autotvm.apply_history_best(filename):
+        with tvm.target.create("llvm"):
+            return template()
